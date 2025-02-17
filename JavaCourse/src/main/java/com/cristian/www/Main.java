f88bbf0ea9b93914c797c06184325ed48964dcf5
cristian vega <cris02vega@gmail.com>
2025-02-16 19:09:16 -0600

    add comment to the class main

--- JavaCourse/src/main/java/com/cristian/www/Main.java
+++ JavaCourse/src/main/java/com/cristian/www/Main.java
@@ -2,6 +2,8 @@ package com.cristian.www;
 
 public class Main {
     public static void main(String[] args) {
+
+        //este es el primer cambio para mejorar la logica de programacion
         System.out.println("Hello world!");
     }
 }
